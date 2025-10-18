13bd974b54919622cf968bf9b982c5733a1102a5
Lone <cullen.zh@gmail.com>
2025-10-18 17:34:39 +0800

    Update donate.tsx

--- src/components/toolbar/menu/items/donate.tsx
+++ src/components/toolbar/menu/items/donate.tsx
@@ -5,7 +5,7 @@ import { Item } from '../item';
 export function Donate() {
   return (
     <Item
-      href="https://buymeacoffee.com/remvze"
+      href="https://buymeacoffee.com/llone"
       icon={<SiBuymeacoffee />}
       label="Buy Me a Coffee"
     />
